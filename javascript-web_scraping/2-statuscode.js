9999fde040fd36e14a845d9059669f39ae271aee
lenne sebastien <sebastienlenne@gmail.com>
2023-09-18 21:00:13 +0200

    update file for task 2 for project javascript-web_scraping

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,13 @@
-#!/usr/bin/node
+#!/usr/local/opt/node@18/bin/node
 
 const request = require('request');
 
 const url = process.argv[2];
 
-request.get(url)
-  .then((response) => {
-    console.log(`code: ${response.status}`);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+request.get(url, (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
