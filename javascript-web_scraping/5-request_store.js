312fd66ac2c66ad8a25e6968a9316ddf046c7f7e
lenne sebastien <sebastienlenne@gmail.com>
2023-09-19 10:31:49 +0200

    add file for task 5 for project javascript-web_scraping

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,29 @@
+#!/usr/local/opt/node@18/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Send a GET request to the specified URL
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  // Check if the request was successful (status code 200)
+  if (response.statusCode === 200) {
+    // Write the response body to the specified file
+    fs.writeFile(filePath, body, 'utf-8', function (err) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`Content saved to ${filePath}`);
+      }
+    });
+  } else {
+    console.error(`Request failed with status code: ${response.statusCode}`);
+  }
+});
