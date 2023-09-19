204b5be33fe9eefd208fc949506bf0bfe042dea5
lenne sebastien <sebastienlenne@gmail.com>
2023-09-19 09:29:03 +0200

    update file for task 4 for project javascript-web_scraping

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,25 +1,25 @@
-#!/usr/local/opt/node@18/bin/node
+#!/usr/bin/node
 
 const request = require('request');
 
 const apiUrl = process.argv[2];
-const characterId = '18'; // Wedge Antilles character ID
+const characterId = '18'; // ID du personnage Wedge Antilles
 
-// Send a GET request to the Star Wars API films endpoint
+// Envoye une requête GET à l'API Star Wars pour obtenir la liste des films
 request(apiUrl, function (error, response, body) {
   if (error) {
     console.error(error);
     return;
   }
 
-  // Parse the JSON response
+// Analyser la réponse JSON
   const filmsData = JSON.parse(body);
 
-  // Filter the films where Wedge Antilles appears
-  const filmsWithWedgeAntilles = filmsData.results.filter((film) => {
+  // Filtrer les films où Wedge Antilles apparaît
+    const filmsWithWedgeAntilles = filmsData.results.filter((film) => {
     return film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`);
   });
 
-  // Print the count of films with Wedge Antilles
-  console.log(filmsWithWedgeAntilles.length);
+  // print le nombre de films avec Wedge Antilles
+    console.log(filmsWithWedgeAntilles.length);
 });
