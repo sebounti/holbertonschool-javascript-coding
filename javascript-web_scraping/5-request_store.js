c4f2db71ac55720695da6d6f72383e9567548763
lenne sebastien <sebastienlenne@gmail.com>
2023-09-22 22:19:56 +0200

    update file for task 5 for project javascript-web_scraping

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,29 +1,14 @@
-#!/usr//bin/node
+#!/usr/bin/node
 
 const request = require('request');
 const fs = require('fs');
-
-const url = process.argv[2];
+const apiURL = process.argv[2];
 const filePath = process.argv[3];
 
-// Send a GET request to the specified URL
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  // Check if the request was  successful (status code 200)
-  if (response.statusCode === 200) {
-    // Write the response body to the specified file
-    fs.writeFile(filePath, body, 'utf-8', function (err) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(`Content saved to ${filePath}`);
-      }
-    });
+request.get(apiURL, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    fs.writeFileSync(filePath, body, 'utf8');
   } else {
-    console.error(`Request failed with status code: ${response.statusCode}`);
+    console.error(error || `Erreur: Code d'état ${response.statusCode}`);
   }
 });
