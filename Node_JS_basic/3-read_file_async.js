cc9e0beaf41088f30d57427a8bbd9991117a7af3
lenne sebastien <sebastienlenne@gmail.com>
2023-10-09 23:34:12 +0200

    add file task 3 for project node js basic

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.split('\n');
+        lines.shift();
+        lines.filter((line) => line !== '');
+        const fields = {};
+        for (const line of lines) {
+          const [firstName, , , field] = line.split(',');
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstName);
+        }
+        console.log(`Number of students: ${lines.length}`);
+        for (const [field, students] of Object.entries(fields)) {
+          console.log(
+            `Number of students in ${field}: ${
+              students.length
+            }. List: ${students.join(', ')}`,
+          );
+        }
+        resolve();
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
